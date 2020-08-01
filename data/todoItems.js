086d71c8eda5c2dd0d8e76db4e61fd41f39b9960
Soham <65823748+codingiscool121@users.noreply.github.com>
2020-08-01 13:48:17 -0400

    Add files via upload

---
+++ data/todoItems.js
@@ -0,0 +1,10 @@
+var todoListItems = [
+	"Check breakmycipher for intercepted message",
+	"Decrypt message",
+	"Go to terminal",
+	"Hack the hotel's wi-fi",
+	"Send correct phishing email to target",
+	"Log into the target's fakebook using username and password",
+	"Log into the target's bank account",
+	"Transfer money to buy weapons"
+]
---
+++ data/webpages.js
@@ -0,0 +1,51 @@
+webpage_data = [
+  {
+    "title": "Cipher",
+    "name": "Break My Cipher",
+    "url": "https://www.breakmycipher.io",
+    "src": "ciphertext/ciphertext.html",
+    "image": "misc/images/breakmycipher.png",
+    "instructions": "You intercepted an encrypted message between the targeted drug lord and one of their super wealthy,"
+                + " valued customers. Break the cipher to get more information on the client. Be careful though, if there are too many"
+                + " attempts you will leave a trail for the drug lord to trace back to you!",
+  },
+  {
+    "title": "Fakebook",
+    "name": "Fakebook",
+    "url": "https://www.fakebook.com",
+    "src": "fakebook/fakebook.html",
+    "image": "misc/images/fakebook.png",
+    "instructions": "Enter the username and password for Fakebook.",
+  },
+  {
+    "title": "Bank",
+    "name": "Galactic Bank",
+    "url": "https://www.galacticbank.com",
+    "src": "bank/login.html",
+    "image": "misc/images/galacticbank.png",
+    "instructions": "Your job is to hack into this person's bank account. Use the URL bar to navigate to different webpages.",
+  },
+  {
+    "title": "WifiCrack",
+    "name": "WifiCrack",
+    "url": "https://www.wificrack.com",
+    "src": "wifihack/aircrack2.html",
+    "image": "misc/images/wificrack.png",
+    "instructions": "<ul>" +
+   "<li>  Your next hack takes place at the scheduled meeting time </li>" +
+   "<li> You need to create a rogue wifi hotspot that is named similarly to the official free wifi at the meeting place </li>" +
+   "<li> Your goal is to attract people to access to the internet through your wifi </li>" +
+   "</ul>",
+
+
+
+  },
+  {
+    "title": "Zmail",
+    "name": "Zmail",
+    "url": "https://www.zmail.com",
+    "src": "email/compose.html",
+    "image": "misc/images/zmail.png",
+    "instructions": "Your job is to choose good phishing email (subject options) to get the drug lord's zmail password. Make sure to select the phishing email which will not raise any suspision. Use the credentials to access their Fakebook page.",
+  },
+];
